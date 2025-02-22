37addf7a1d4a8976f13b9bfe2da265c7e1696127
YuriiPatriuk12 <Patriuk.Yurii@LLL.kpi.ua>
2025-02-22 15:17:59 +0200

    Add high priority task

--- Lab1/src/Ball.java
+++ Lab1/src/Ball.java
@@ -10,16 +10,11 @@ class Ball {
     private int y = 0;
     private int dx = 2;
     private int dy = 2;
+    private Color color;
 
-    public Ball(Component c) {
+    public Ball(Component c, Color color) {
         this.canvas = c;
-        if (Math.random() < 0.5) {
-            x = new Random().nextInt(this.canvas.getWidth());
-            y = 0;
-        } else {
-            x = 0;
-            y = new Random().nextInt(this.canvas.getHeight());
-        }
+        this.color = color;
     }
 
     public static void f() {
@@ -27,7 +22,7 @@ class Ball {
     }
 
     public void draw(Graphics2D g2) {
-        g2.setColor(Color.darkGray);
+        g2.setColor(color);
         g2.fill(new Ellipse2D.Double(x, y, XSIZE, YSIZE));
     }
 
--- Lab1/src/BounceFrame.java
+++ Lab1/src/BounceFrame.java
@@ -7,36 +7,70 @@ public class BounceFrame extends JFrame {
     private BallCanvas canvas;
     public static final int WIDTH = 450;
     public static final int HEIGHT = 350;
+    private static final int EXAMPLE_BLUE_BALLS = 50;
 
     public BounceFrame() {
         this.setSize(WIDTH, HEIGHT);
-        this.setTitle("Bounce programm");
+        this.setTitle("Bounce program");
         this.canvas = new BallCanvas();
         System.out.println("In Frame Thread name = " + Thread.currentThread().getName());
+
         Container content = this.getContentPane();
         content.add(this.canvas, BorderLayout.CENTER);
+
         JPanel buttonPanel = new JPanel();
         buttonPanel.setBackground(Color.lightGray);
-        JButton buttonStart = new JButton("Start");
+        JButton buttonRed = new JButton("Add Red Ball");
+        JButton buttonBlue = new JButton("Add Blue Ball");
+        JButton buttonExample = new JButton("Example");
         JButton buttonStop = new JButton("Stop");
-        buttonStart.addActionListener(new ActionListener() {
+
+        buttonRed.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                Ball b = new Ball(canvas);
-                canvas.add(b);
-                BallThread thread = new BallThread(b);
-                thread.start();
-                System.out.println("Thread name = " + thread.getName());
+                createBall(Color.RED, Thread.MAX_PRIORITY);
             }
         });
+
+        buttonBlue.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                createBall(Color.BLUE, Thread.MIN_PRIORITY);
+            }
+        });
+
+        buttonExample.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                for(int i = 0; i < EXAMPLE_BLUE_BALLS; i++)
+                {
+                    createBall(Color.BLUE, Thread.MIN_PRIORITY);
+                }
+
+                createBall(Color.RED, Thread.MAX_PRIORITY);
+            }
+        });
+
         buttonStop.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.exit(0);
             }
         });
-        buttonPanel.add(buttonStart);
+        buttonPanel.add(buttonRed);
+        buttonPanel.add(buttonBlue);
+        buttonPanel.add(buttonExample);
         buttonPanel.add(buttonStop);
         content.add(buttonPanel, BorderLayout.SOUTH);
+    }
+
+    private void createBall(Color color, int priority)
+    {
+        Ball b = new Ball(canvas, color);
+        canvas.add(b);
+        BallThread thread = new BallThread(b);
+        thread.setPriority(priority);
+        thread.start();
+        System.out.println("Thread name = " + thread.getName());
     }
 }
